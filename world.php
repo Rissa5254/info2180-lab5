73d66eadb4f97e6469af33d382504c9c28dd4d98
Marissa O'Meally <marissaomeally21@gmail.com>
2025-11-13 01:23:54 -0500

    Updated files

--- world.php
+++ world.php
@@ -1,17 +1,36 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// Check if the country parameter exists
+if (isset($_GET['country'])){
+  $country = $_GET['country'];
+
+  // Preventing SQL Injection
+  $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+  $stmt->execute(['country' => "%$country%"]);
+
+  // Fetch results
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+} else{
+  echo "Please provide a country using ?country=CountryName";
+  exit;
+}
 
 ?>
+<!-- Display Results--->
 <ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
+<?php if (!empty($results)): ?>  
+  <?php foreach ($results as $row): ?>
+    <li><?= htmlspecialchars($row['name']) . ' is ruled by ' . htmlspecialchars($row['head_of_state']); ?></li>
+  <?php endforeach; ?>
+<?php else: ?>
+  <li>No Results Found.</li>
+<?php endif; ?>
 </ul>
