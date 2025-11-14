0babf045c0d277eb88a35de4b150abf2843341b6
Marissa O'Meally <marissaomeally21@gmail.com>
2025-11-14 02:47:56 -0500

    Updated files

---
+++ world.js
@@ -0,0 +1,24 @@
+// Wait for the page to finish load
+window.addEventListener("DOMContentLoaded", function(){
+    const button = document.getElementById("lookup");
+    const results = document.getElementById("result");
+
+    // User clicks the Lookup Button
+    button.addEventListener("click", function(){
+          // prevent page from reloading
+        
+        // Santize user input
+        const countryData = document.getElementById("country").value;
+        const country = countryData.replace(/</g, "&lt;").replace(/>/g, "&gt;").trim();
+
+        fetch("world.php?country=" + encodeURIComponent(country))  // AJAX request
+            .then(response => response.text())
+            .then(data => {
+                data = results.innerHTML;
+            })
+            .catch(error => {
+                console.error("Error:", error);
+                results.innerHTML = "<p>Erorr in fetching country.</p>";  // Error alert message
+            });
+    });
+});
